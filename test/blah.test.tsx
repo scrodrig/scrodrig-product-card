1b94b0ae3a96c2db2c5abd4f561561da1f9c3dc7
Schubert Rodriguez Quezada <schubert.quezada@babelgroup.com>
2024-04-05 15:46:37 +0100

    Version 1.0.0-rc ready

--- test/blah.test.tsx
+++
@@ -1,9 +0,0 @@
-import * as ReactDOM from 'react-dom';
-
-describe('it', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    // ReactDOM.render(<Thing />, div);
-    ReactDOM.unmountComponentAtNode(div);
-  });
-});
